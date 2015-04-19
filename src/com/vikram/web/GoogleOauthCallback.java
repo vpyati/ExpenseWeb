c37e29f0cc1c8710f15fdc6379c3b625534989c7
Vikram Pyati <vipyati@ebay.com>
2015-04-19 18:56:25 +0530

    Oauthcallback

--- src/com/vikram/web/GoogleOauthCallback.java
+++ src/com/vikram/web/GoogleOauthCallback.java
@@ -1,28 +1,16 @@
 package com.vikram.web;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+
+import com.vikram.openconnect.login.controller.OpenconnectCallbackController;
 
 @Controller
 @RequestMapping("/oauth2callback")
-public class GoogleOauthCallback {
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView callBack(@RequestParam("code")String code, @RequestParam("state")String state, HttpServletResponse response){
-		
-		response.addCookie(new Cookie("authorization_code", code));
-		
-		RedirectView view = new RedirectView("/dashboard",true);
-		
-		return new ModelAndView(view);
-		
-	}
+public class GoogleOauthCallback extends OpenconnectCallbackController {
 
+	@Override
+	public String getRedirectView() {
+		return "/dashboard" ;
+	}
 }
