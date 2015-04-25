ee08bc8fbd8b2b4faaa66e3005880627c75073d1
Vikram Pyati <vipyati@ebay.com>
2015-04-25 12:09:14 +0530

    Changing versions

--- src/com/vikram/configuration/AccessToken.java
+++ src/com/vikram/configuration/AccessToken.java
@@ -7,23 +7,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.util.WebUtils;
 
 import com.vikram.openconnect.login.IAccessToken;
+import com.vikram.openconnect.login.providers.OAuthProvider;
 
 public class AccessToken implements IAccessToken {
 
 	private static String COOKIE_NAME = "access_token";
 
 	@Override
-	public void setAccessToken(String accessToken, HttpServletResponse response) {
+	public void setAccessToken(String accessToken, HttpServletResponse response,OAuthProvider provider) {
 		response.addCookie(new Cookie(COOKIE_NAME,accessToken));	
 	}
 
 	@Override
-	public String getAccessToken(HttpServletRequest request) {
+	public String getAccessToken(HttpServletRequest request,OAuthProvider provider) {
 		Cookie accessToken = WebUtils.getCookie(request, COOKIE_NAME);
 		return accessToken==null?"":accessToken.getValue();
 	}
-
-	
-	
-
 }
