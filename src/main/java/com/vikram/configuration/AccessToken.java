c8fdda7e84bdf90537f2aaeb726f4faddf6dafa3
Vikram Pyati <vipyati@ebay.com>
2015-04-26 18:14:04 +0530

    Refactoring and adding logger

--- src/main/java/com/vikram/configuration/AccessToken.java
+++ src/main/java/com/vikram/configuration/AccessToken.java
@@ -4,6 +4,8 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.util.WebUtils;
 
 import com.vikram.openidconnect.login.core.providers.OAuthProvider;
@@ -12,14 +14,18 @@ import com.vikram.openidconnect.login.web.IAccessToken;
 public class AccessToken implements IAccessToken {
 
 	private static String COOKIE_NAME = "access_token";
+	
+	private Logger logger = LoggerFactory.getLogger(AccessToken.class);
 
 	@Override
 	public void setAccessToken(String accessToken, HttpServletResponse response,OAuthProvider provider) {
+		logger.info("Setting accesstoken");
 		response.addCookie(new Cookie(COOKIE_NAME,accessToken));	
 	}
 
 	@Override
 	public String getAccessToken(HttpServletRequest request,OAuthProvider provider) {
+		logger.info("Fetching access token from cookie");
 		Cookie accessToken = WebUtils.getCookie(request, COOKIE_NAME);
 		return accessToken==null?"":accessToken.getValue();
 	}
